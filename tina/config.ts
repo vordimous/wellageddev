c348899410a237a451f54aca605de78745e99e70
AJ <vordimous@outlook.com>
2024-09-15 19:29:19 -0400

    use tina build

--- tina/config.ts
+++ tina/config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
       {
         label: 'Pages',
         name: 'pages',
-        path: 'content/',
+        path: 'content/*.md',
         fields: [
           {
             type: "datetime",
